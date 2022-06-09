b9a378025eae445086a3ae8f1a3c1b2ed3c5dc8d
cyyc1498 <78880455+cyyc1498@users.noreply.github.com>
2022-06-09 00:54:07 -0400

    added definition component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dictionary from './dictionary';
+import Definition from './definition';
 
 function App() {
   return (
-    <div className="App">
-      <div className="initial-page row">
+    <div className="App"> 
+    <div className="container">
+      <div className="row">
           <span className='col'><h1 className='title'>Dictionary</h1></span>
           <span className='col'><Dictionary/></span>
         </div>
-      <div className="container">
-        
+     
+       
       </div>
       
       
--- src/dictionary.js
+++ src/dictionary.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React,{useState} from "react";
+import Definition from "./definition";
 
 export default function Dictionary(){
 
@@ -16,11 +17,13 @@ function handleSubmit(response){
 }
 axios.get(apiUrl).then(handleSubmit)
 return (
+    
     <div>
     <form>
         <input type="text" className="search-bar" onChange={getWord}></input>
         <input type="submit" value="Search" className="search-button" onSubmit={handleSubmit}></input>
     </form>
+    <Definition word={word}/>
     </div>
 )
 }
