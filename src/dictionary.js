331b140f400b7c106c73be1ae8e25d5feedd5afe
cyyc1498 <78880455+cyyc1498@users.noreply.github.com>
2022-06-09 00:30:36 -0400

    added API URL and added handleSubmit function, deleted default text in index.html

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,25 +1,25 @@
+import axios from "axios";
 import React,{useState} from "react";
 
 export default function Dictionary(){
 
 let [word,setWord] = useState("")
 
-
-function handleSubmit(){
-    
-}
-
-let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-
 function getWord(event){
 setWord(event.target.value)
 }
 
+let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+
+function handleSubmit(response){
+    console.log(response.data)
+}
+axios.get(apiUrl).then(handleSubmit)
 return (
     <div>
     <form>
         <input type="text" className="search-bar" onChange={getWord}></input>
-        <input type="submit" value="Search" className="search-button"></input>
+        <input type="submit" value="Search" className="search-button" onSubmit={handleSubmit}></input>
     </form>
     </div>
 )
