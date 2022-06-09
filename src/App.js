aa2d85a1bedf162eda819f904fa7493b25dc0811
cyyc1498 <78880455+cyyc1498@users.noreply.github.com>
2022-06-08 22:33:26 -0400

    imported fonts from google and did some simple initial CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Dictionary from './dictionary';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>hello</h1>
-       
+        <div className="initial-page">
+        <h1 className='title'>Dictionary</h1>
+        <Dictionary/>
+       </div>
       </div>
       
       
--- src/dictionary.js
+++ src/dictionary.js
@@ -1,1 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
+
+export default function Dictionary(){
+
+let [word,setWord] = useState("")
+
+function getWord(event){
+setWord(event.target.value)
+}
+
+return (
+    <div>
+    <form>
+        <input type="text" className="search-bar" onChange={getWord}></input>
+        <input type="submit" value="Search" className="search-button"></input>
+    </form>
+    </div>
+)
+}
