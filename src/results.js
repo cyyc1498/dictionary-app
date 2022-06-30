c5a2f1cb6d8c56b7ae95b6b99da19efbe89d3f9d
cyyc1498 <78880455+cyyc1498@users.noreply.github.com>
2022-06-30 18:22:45 -0400

    created phonetics component

--- src/results.js
+++ src/results.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Meaning from "./meaning";
-
+import Phonetics from "./phonetics";
 
 export default function Results(props){
     console.log(props.data)
     if(props.data){
     return(
         <div className="results">
-        <h2 className="cap searched-word text-center"><strong>{props.data.word}</strong> - {props.data.phonetic}</h2>
+        <h2 className="cap searched-word text-center"><strong>{props.data.word}</strong></h2>
+        <Phonetics phonetics={props.data.phonetic}/>
         <div>{props.data.meanings.map(function(meaning,index){
             return(
                 <div key={index}>
